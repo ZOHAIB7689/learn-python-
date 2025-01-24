a862e1ed11b2d011d10a5136a04d40e5a3815a06
ZOHAIB7689 <bzohaib754@gmail.com>
2025-01-24 10:11:56 +0500

    main

---
+++ 04-numbers/basic01.py
@@ -0,0 +1,29 @@
+# python is king of numbers game
+
+# basic operations on numbers
+a = 2 
+y = 3 
+z = 4
+
+# plus 
+print (a + y) 
+# Answer is 5
+# minus
+print ( z-a )
+# Answer is 2
+
+# there are more operations like multiplication, division, modulus, floor division, power etc.
+
+# this becomes a tuple
+x = a , y ,z 
+
+a +1 , y* 2 
+
+# comparison operations
+# comparison operations
+print(a == y)  # False
+print(a != y)  # True
+print(a > y)   # False
+print(a < y)   # True
+print(a >= y)  # False
+print(a <= y)  # True
